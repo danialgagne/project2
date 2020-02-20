bcd1e082bbd7973bf21f497260c428e6568d2753
danialgagne <gagne.danial@gmail.com>
2020-02-19 18:22:56 -0600

    load environment variables with a .env file and update gitignore accordingly

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
+load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
