4a5134820103eb71797f2362f4aa2b8e2f930bab
danialgagne <gagne.danial@gmail.com>
2020-02-24 13:47:59 -0600

    submitting form sets display name in local storage

---
+++ static/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // show display name if set
+    if (localStorage.getItem('display-name')) {
+        document.querySelector('body').innerHTML = `Hello, ${localStorage.getItem('display-name')}`
+    }
+
+    // allow user to set display name
+    document.querySelector('#new-display-name').onsubmit = () => {
+        display_name = document.querySelector('#display-name').value;
+        localStorage.setItem('display-name', display_name);
+
+        return false;
+    }
+})
